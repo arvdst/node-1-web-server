d23f8b56c4b209143987d53cd30f7313acc57ee0
drruvw <drruvw@gmail.com>
2018-02-25 23:53:23 +0530

    New commit, projects

--- server.js
+++ server.js
@@ -56,6 +56,13 @@ app.get('/about', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Projects Page',
+		welcomeMessage: 'Welcome to Projects Page'
+	});
+});
+
 app.get('/bad', (req, res) => {
 	res.send({
 		errorMessage: 'Unable to fulfill this request'
